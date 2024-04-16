13475fb250c2bb3baf3d221967e860cee9e84baa
kewen <kauakewen1010@hotmail.com>
2024-04-16 17:28:11 -0300

    Update 02-arrays.js

--- Js/02-arrays.js
+++ Js/02-arrays.js
@@ -13,3 +13,12 @@ console.log(alunos[0]); //Willian
 const lista = ["Pudim", "Dj Arana", "Lana Del Rey", "Deceit", "GTA", "Dead By Daylight", "Robótica"];
 
 console.log(`Meu artista fav é ${lista[1]}, meu jogo fav é ${lista[4]} e curto ${lista[6]}`);
+
+// MATRIZ (array com mais de uma dimensão)
+
+const tecnologias = [
+    ["HTML5", "CSS3", "JavaScript"],
+    ["PHP", "Node.Js", "SQL", "Python", "Firebase"]
+];
+
+console.log(tecnologias[0][1]);
