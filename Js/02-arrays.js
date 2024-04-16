b3d2db3194b3316c376fabf397857db3473fee1c
kewen <kauakewen1010@hotmail.com>
2024-04-16 17:14:29 -0300

    Create 02-arrays.js

---
+++ Js/02-arrays.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const alunos = ["Willian", "Vinicius", "Marli", "Duda", "Gabriel"];
+
+// Exibindo a estrutura do array (somente no console)
+console.log(alunos);
+
+// Acessando individualmente alguns alunos
+console.log(alunos[1]); //Vinicius
+console.log(alunos[3]); // Duda
+console.log(alunos[0]); //Willian
+
+const lista = ["Pudim", "Dj Arana", "Lana Del Rey", "Deceit", "GTA", "Dead By Daylight", "Robótica"];
+
+console.log(`Meu artista fav é ${lista[1]}, meu jogo fav é ${lista[4]} e curto ${lista[6]}`);
