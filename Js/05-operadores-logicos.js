c94c2719697a57cca43f779bdf962b2b4f6bb3d7
kewen <kauakewen1010@hotmail.com>
2024-04-18 15:28:57 -0300

    Update 05-operadores-logicos.js

--- Js/05-operadores-logicos.js
+++ Js/05-operadores-logicos.js
@@ -18,3 +18,23 @@ if(mediaFinal >= mediaMinima && faltas<= limiteDeFaltas){
 }
 
 console.log(resultado);
+
+console.log("-------------------");
+
+let idade = 25;
+
+// Operador OU/OR (||)  obs: O símbolo | é chamado de PIPE
+if (idade >= 18|| idade < 12){
+    console.log("Você é adulto ou criança");
+} else {
+    console.log("Você é um adolescente");
+}
+
+
+let onibus = 2203; //2203 ou 2019
+
+if(onibus==2203 || onibus==2019){
+    console.log("Passa no Senac, embarque!");
+} else {
+    console.log("Não passa no Senac, aguarde!");
+}
