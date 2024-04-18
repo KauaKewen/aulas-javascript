f79f780362d8514cb82b2b6ce7659957d356b8c2
kewen <kauakewen1010@hotmail.com>
2024-04-18 16:23:28 -0300

    Update 04-condicionais.js

--- Js/04-condicionais.js
+++ Js/04-condicionais.js
@@ -53,3 +53,19 @@ if(idade >= 60){
 }
 
 console.log(`${usuario} tem ${idade} anos e é ${texto3}`);
+
+console.log("--------");
+
+// Condicional switch/case
+let opcao = 1;
+let mensagem;
+
+switch(opcao){
+    case 1: mensagem = "Informações"; break;
+    case 2: mensagem = "Reclamações"; break;
+    case 3: mensagem = "Elogios"; break;
+    default: mensagem = "Falar com atendente"; break;
+}
+
+console.log("Opção escolhida: "+opcao);
+console.log("Mensagem: "+mensagem);
