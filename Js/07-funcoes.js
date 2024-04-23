8f40a4d693b5e11a0e320694e438765bd3cd6400
kewen <kauakewen1010@hotmail.com>
2024-04-23 16:17:51 -0300

    Create 07-funcoes.js

---
+++ Js/07-funcoes.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// Sintaxe 1: função anônima
+const exemplo1 = function(){
+    console.log("Função anônima");
+};
+
+// Chamando/invocando a função
+exemplo1();
+
+// Sintaxe 2: função nomeada/declarada
+function exemplo2(){
+    console.log("Função nomeada/declarada");
+}
+
+exemplo2();
