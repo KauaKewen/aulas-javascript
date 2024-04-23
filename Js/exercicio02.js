ec38da5aaa70efd22359b8c831dcae48c2784421
kewen <kauakewen1010@hotmail.com>
2024-04-23 16:17:48 -0300

    Create exercicio02.js

---
+++ Js/exercicio02.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+const clientes = [
+    {
+        id: 1,
+        nome: "Goku"
+    },
+    {
+        id: 2,
+        nome: "Naruto"
+    },
+    {
+        id: 3,
+        nome: "Shiryu"
+    }
+];
+
+for(const cliente of clientes){
+    console.log(`Cliente: ${cliente.nome} (id: ${cliente.id})`);
+}
